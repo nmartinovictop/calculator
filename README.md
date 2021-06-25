99e50c89ec785cda41f93e9b3a56cd038e55f62f
Nick Martinovic TOP <nmartinovictop@gmail.com>
2021-06-25 21:41:26 +0900

    working on calc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # calculator
 https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator
 
-**Next Action: Style the calculator a little bit, then add JS**
+**Next Action: Try to add JS**
 
 - 2021-06-20: I was able to build the calculator skeleton and build the basic JS functions.  
--- script.js
+++ script.js
@@ -16,20 +16,41 @@ function divide(a,b) {
 }
 
 function operate(operator,num1,num2) {
-    if (operator == 'add') {
+    if (operator == '+') {
         return add(num1,num2)
-    } else if (operator == 'subtract') {
+    } else if (operator == '-') {
         return subtract(num1,num2)
-    } else if (operator == 'multiply') {
+    } else if (operator == '*') {
         return multiply(num1,num2)
     } else {
         return divide(num1,num2)
     }
 }
 
-let divs = document.querySelectorAll('.keypad')
+let nums = document.querySelectorAll('.numpad')
+let operators = document.querySelectorAll('.operator')
+let num1 = '';
+let num2 = '';
+let currentOp = '';
 
-divs.forEach( div => {
-    div.addEventListener('click', () => 
-    console.log(div.textContent))
-})
+nums.forEach( num => {
+    num.addEventListener('click', numCollector)
+});
+
+operators.forEach (op => {
+    op.addEventListener('click',() => currentOp += op.textContent)
+});
+
+
+function numCollector() {
+    if (currentOp === '') { 
+        num1 += this.textContent
+    } else {
+        num2 += this.textContent
+    }
+}
+
+function calculate() {
+   return operate(currentOp,num1,num2)
+
+}
