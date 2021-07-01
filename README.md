a8d67ed729af3144f124a05bc16471963bbcf0a9
Nick Martinovic TOP <nmartinovictop@gmail.com>
2021-07-01 20:31:14 +0900

    calcuaor

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/cal
 
 **Next Action: Try to add JS**
 
+- 2021-06-30: I've got a nice little display that shows history.  I need to figure out how to handle clear events.  Also double ops
 - 2021-06-28: I can now accept decimals and clear.  I would like to make the display better.
 - 2021-06-27: Basic calculator is working for whole numbers.  I need to fix it let it accept decimals
 - 2021-06-20: I was able to build the calculator skeleton and build the basic JS functions.  
--- script.js
+++ script.js
@@ -34,13 +34,17 @@ let display = document.querySelector('.display')
 let content = document.createElement('p');
 let decimal = document.querySelector('.decimal')
 let clear = document.querySelector('.clear')
+let entries = document.querySelector('.entries')
+let data = document.createElement('p')
 display.appendChild(content)
+entries.appendChild(data)
 
 
 let num1 = '';
 let num2 = '';
 let currentOp = '';
 let result = '';
+// let data = ''
 
 
 decimal.addEventListener('click',addDecimal)
@@ -48,8 +52,10 @@ decimal.addEventListener('click',addDecimal)
 function addDecimal() {
     if (currentOp === '') {
         num1 += this.textContent
+        data.textContent += this.textContent
     } else {
         num2 += this.textContent
+        data.textContent += this.textContent
     }
 
     decimal.removeEventListener('click',addDecimal)
@@ -65,17 +71,22 @@ nums.forEach( num => {
 operators.forEach (op => {
     op.addEventListener('click',() => {
     currentOp += op.textContent
+    data.textContent += op.textContent
     decimal.addEventListener('click',addDecimal)})
     
 
 });
 
 
+
 function numCollector() {
     if (currentOp === '') { 
         num1 += this.textContent
+        data.textContent += this.textContent
+        
     } else {
         num2 += this.textContent
+        data.textContent += this.textContent
     }
 
 }
@@ -86,6 +97,7 @@ function calculate() {
     num2 = '';
     currentOp = ''
     content.textContent = result;
+    data.textContent += `= ${result}`
     return result
 }
 
@@ -93,6 +105,7 @@ clear.addEventListener('click',function() {
     num1 = '';
     num2 = '';
     currentOp = '';
+    data.textContent = ''
     decimal.addEventListener('click',addDecimal);
 })
 
